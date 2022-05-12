7af149bb4c8d6e88886b79966bb7ebfa47c9666a
Mike van der Vrugt <info@mikevandervrug.nl>
2022-05-12 16:55:45 +0200

    A lot of layout done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
-(function() {
-    document.querySelectorAll('.formulaType input').addEventListener('click', (e) => {
-        let target = e.target;
-        let parent = target.parentNode;
-
-        document.querySelectorAll('.formulaType').forEach((el) => {
-            el.classList.remove('active');
+jQuery(function($) {
+    $("input[name=type]").change(function() {
+        $("input[name=type]").each(function() {
+            $(this).parent().removeClass("active");
         });
-
-        parent.classList.add('active');
-
+        $(this).parent().addClass("active");
     });
-})();
+
+    $("#cva-select").change(function() {
+        let target = $(this).val();
+        console.log(target);
+        if (target === "-1") {
+            $(".custom-cva").show();
+        } else {
+            $(".custom-cva").hide();
+        }
+    });
+});
