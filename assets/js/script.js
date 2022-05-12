b55b1e5a1396a9bb300091f6b5a5789b020dfec0
Sebastiaan Colmen <skolmschate@hotmail.nl>
2022-05-12 23:31:49 +0200

    changed some bootstrap stuff made the calculation work

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,42 @@ CVAselect.addEventListener("change", () => {
     customCVA.style.display = "none";
   }
 });
+
+const checkInput = document.querySelector("#calculate");
+const inMargin = document.querySelector("#in-margin");
+const outMargin = document.querySelector("#out-margin");
+
+checkInput.addEventListener("click", () => {
+  const blood1 = parseFloat(document.querySelector("#blood1").value);
+  const blood2 = parseFloat(document.querySelector("#blood2").value);
+  const cvi = parseFloat(document.querySelector("#cvi").value);
+  const cva = parseFloat(document.querySelector("#cva-custom").value);
+  const z = 1.95
+
+  const rcv = getRCV(cva, cvi, z);
+  const margin = rcv / 2;
+
+  const innerBound = blood1 - margin;
+  const outerBound = blood1 + margin;
+
+  console.log("rcv: ", rcv);
+  console.log("margin: ", margin);
+  console.log("blood1: ", blood1);
+  console.log("innerBound: ", innerBound);
+  console.log("outerBound: ", outerBound);
+  console.log("blood2: ", blood2);
+
+  if(blood2 > innerBound && blood2 < outerBound) {
+    inMargin.style.display = "";
+    outMargin.style.display = "none";
+  }
+  else
+  {
+    inMargin.style.display = "none";
+    outMargin.style.display = "";
+  }
+});
+
+function getRCV(cva, cvi, z){
+  return Math.sqrt(cva**2 + cvi**2) * Math.sqrt(2) * z
+}
